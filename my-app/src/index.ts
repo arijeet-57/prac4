9da299a39d4179c7f941e66900bcbdf1d0463073
Arijeet Roy <blakelabs57@gmail.com>
2025-09-26 22:48:07 +0530

    Servers is Cloudflare

--- my-app/src/index.ts
+++ my-app/src/index.ts
@@ -1,18 +1,23 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		console.log(request.body);
+		console.log(request.headers);
+		console.log(request.method);
+		var uri = request.url.replace(/^https:\/\/.*?\//gi, "/");
+		console.log(uri);
+
+		if (request.method === "GET") {
+			if (uri === "/users") {
+				// handle the request and write the logic here
+			}
+			return Response.json({
+				msg : "You sent a GET Request"
+			})
+		}else{
+			return Response.json({
+				msg: "No GET request sent"
+			})
+		}
 	},
-} satisfies ExportedHandler<Env>;
+} 
