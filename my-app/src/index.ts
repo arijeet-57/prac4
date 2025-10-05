7a167974ab76376a0afc08ed61a68db6956f3815
Arijeet Roy <blakelabs57@gmail.com>
2025-10-05 20:14:57 +0530

    Hono

--- my-app/src/index.ts
+++ my-app/src/index.ts
@@ -1,23 +1,9 @@
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		console.log(request.body);
-		console.log(request.headers);
-		console.log(request.method);
-		var uri = request.url.replace(/^https:\/\/.*?\//gi, "/");
-		console.log(uri);
-
-		if (request.method === "GET") {
-			if (uri === "/users") {
-				// handle the request and write the logic here
-			}
-			return Response.json({
-				msg : "You sent a GET Request"
-			})
-		}else{
 			return Response.json({
 				msg: "No GET request sent"
 			})
-		}
+		
 	},
 } 
